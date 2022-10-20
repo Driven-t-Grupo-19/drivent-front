c238641262796f586f285d309ccaf3d2ef381274
EricChandia <eric.chandia.ti@gmail.com>
2022-10-20 00:43:04 -0300

    PaymentPage started

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -4,15 +4,33 @@ import { GenericButton } from '../../../components/Button';
 
 export default function Payment() {
   const [modality, setModality] = useState('');
+  const [hotel, setHotel] = useState('');
+  const [total, setTotal] = useState(0);
 
   return(
     <PaymentContainer>
       <h1>Ingresso e pagamento</h1>
       <h2>Primeiro, escolha sua modalidade de ingresso</h2>
       <ModalityContainer>
-        <GenericButton onClick={() => setModality('Presential')} clicked={modality === 'Presential' ? true : false }>Presential</GenericButton>
+        <GenericButton onClick={() => setModality('Presential')} clicked={modality === 'Presential' ? true : false }>Presencial</GenericButton>
         <GenericButton onClick={() => setModality('Online')} clicked={modality === 'Online' ? true : false}>Online</GenericButton>
       </ModalityContainer>
+      {
+        modality ? 
+          <>
+            <h2>Ótimo! Agora escolha sua modalidade de hospedagem</h2>
+            <HotelContainer>
+              <GenericButton onClick={() => setHotel('noHotel')} clicked={hotel === 'noHotel' ? true : false }>Sem Hotel</GenericButton>
+              <GenericButton onClick={() => setHotel('withHotel')} clicked={hotel === 'withHotel' ? true : false}>Com Hotel</GenericButton>
+            </HotelContainer>
+          </> : false
+      }
+      {
+        hotel ?
+          <>
+            <h2>Fechado! O total ficou em R$ {total}. Agora é só confirmar:</h2>
+          </> : false
+      }
     </PaymentContainer>
   );
 }
@@ -30,6 +48,11 @@ const PaymentContainer = styled.div`
 `;
 
 const ModalityContainer = styled.div`
+  margin-top: 20px;
+  display: flex;
+`;
+
+const HotelContainer = styled.div`
   margin-top: 20px;
   display: flex;
 `;
